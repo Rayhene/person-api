62454fc34a60c23bbb5754dcae305218c5114b19
Rayhene <rrda@ecomp.poli.br>
2021-06-27 22:12:25 -0300

    list all method

--- src/main/java/com/rayheneranuzia/personapi/controller/PersonController.java
+++ src/main/java/com/rayheneranuzia/personapi/controller/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -21,10 +22,14 @@ public class PersonController {
         this.personService = personService;
     }
 
-
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
     }
 }
