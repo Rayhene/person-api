e1c0863565a0ed92c646eafa43872ac9b54ef0b7
Rayhene <rrda@ecomp.poli.br>
2021-06-28 00:39:35 -0300

    delete by id

--- src/main/java/com/rayheneranuzia/personapi/service/PersonService.java
+++ src/main/java/com/rayheneranuzia/personapi/service/PersonService.java
@@ -42,15 +42,23 @@ public class PersonService {
         return allPeople.stream()
                 .map(personMapper::toDTO)
                 .collect(Collectors.toList());
+    }
 
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = verifyIfExists(id);
 
+        return personMapper.toDTO(person);
     }
 
 
-    public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Person person = personRepository.findById(id)
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+
+        personRepository.deleteById(id);
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
-        
-        return personMapper.toDTO(person);
     }
 }
